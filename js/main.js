29958160e52dde677d617af14f37775f3db3020d
wdard0 <eduardoavaldezjr@gmail.com>
2025-06-03 18:11:05 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('tang inamo pre HAHHAHAHA').split('')
+    const titles = ('pakyo hihi HAHHAHAHA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
